a5c70b739ca9d7e998a4f8359a43d766499dd1ad
David Puckett <david@brightrain.com>
2018-04-27 15:17:13 -0600

    add PopupTeamplate and LayerSwipe and get them working LayerSwipe needed some cleanup including removing an extra map div, adding the correct css, and getting rid of some of the unnecessary dojo html

--- final_project.js/my-app-code.js
+++ final_project.js/my-app-code.js
@@ -1,59 +1,64 @@
 
-   require([
-  "esri/map",
-  "esri/dijit/HomeButton",
-  "esri/arcgis/utils",
-  "dojo/_base/array",
-  "esri/layers/FeatureLayer",
-  "esri/dijit/Scalebar",
-  "dojo/parser",
-  "dojo/domReady!"
-], function(Map, HomeButton, utils, array, FeatureLayer) {
-  
-   map = new Map("map", {
-    basemap: "topo",
-    center: [-88.9733898, 40.063171],
-    zoom: 11
-     
- 
-  });
-  
+require([
+    "esri/map",
+    "esri/dijit/HomeButton",
+    "esri/arcgis/utils",
+    "dojo/_base/array",
+    "esri/layers/FeatureLayer",
+    "esri/dijit/Scalebar",
+    "esri/dijit/PopupTemplate",
+    "esri/dijit/LayerSwipe",
+    "dojo/parser",
+    "dojo/domReady!"
+], function(Map, HomeButton, utils, array, FeatureLayer, Scalebar, PopupTemplate, LayerSwipe) {
 
+    map = new Map("map", {
+        basemap: "topo",
+        center: [-88.89, 40.04],
+        zoom: 13
+    });
 
- 
-//Imagery Dry Year 2012
-  var featureLayer2 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2012/FeatureServer/0");
-  
+    //Imagery Dry Year 2012
+    var popupTemplate = new PopupTemplate({
+        title: "Here Be the TITLE",
+        description: "And some info about this thing like the Grid Code = {GRIDCODE}"
+    });
+    ;
+    var featureLayer2 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2012/FeatureServer/0",{
+        infoTemplate: popupTemplate
+    });
+
     map.addLayer(featureLayer2);
 
-//Imagery Wet Year 2015
-   var featureLayer3 = new             FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2015/FeatureServer/0"); 
-  
+    //Imagery Wet Year 2015
+    var featureLayer3 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2015/FeatureServer/0"); 
+
     map.addLayer(featureLayer3);
-  
-  
+
+
+    var swipeWidget = new LayerSwipe({
+        theme: "LayerSwipe",
+        map: map,
+        layers: [featureLayer3],
+        tool: "vertical",
+        toolClip: 9,
+        toolOffsetTop: null,
+        toolOffsetLeft: null
+    }, "layerSwiper");
+    swipeWidget.startup();
+
     /*  
   //Aquifers
   var featureLayer3 = new FeatureLayer("https://landscape1.arcgis.com/arcgis/rest/services/USA_Aquifers/MapServer/0");
-  
+
     map.addLayer(featureLayer3);
-  
-        
-          var swipeWidget = new LayerSwipe({
-            theme: "LayerSwipe",
-            map: map,
-            layers: [featureLayer2],
-            tool: "vertical",
-            toolClip: 9,
-            toolOffsetTop: null,
-            toolOffsetLeft: null
-            
-         }, "LayerSwipe");
-         swipeWidget.startup();
-         
+
+
+
+
      */
-  
-  /*
+
+    /*
   var scalebar = new Scalebar({
           map: map,
       }, "Scalebar");
@@ -62,20 +67,12 @@
           // use "metric" for kilometers
           scalebarUnit: "dual"
   });
-  
+
   */
-  
-  var home = new HomeButton({
+
+    var home = new HomeButton({
         map: map
-      }, "HomeButton");
-      home.startup();
-  
-  var dojoConfig = {
-        parseOnLoad: true,
-        packages: [{
-          "name": "myModules",
-          "location": location.pathname.replace(/\/[^/]+$/, "") + "/myModules"
-        }]
-      };
-    
-  });
+    }, "HomeButton");
+    home.startup();
+
+});
