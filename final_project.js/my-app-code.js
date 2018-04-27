5e5cbf8dd9b6e22f632c5e730bfc90931a759bc2
ADierker219 <35549428+ADierker219@users.noreply.github.com>
2018-04-26 19:15:07 -0600

    Push From Thurs Night

--- final_project.js/my-app-code.js
+++ final_project.js/my-app-code.js
@@ -1,12 +1,14 @@
 
    require([
   "esri/map",
-  "esri/dijit/LayerSwipe",
+  "esri/dijit/HomeButton",
   "esri/arcgis/utils",
   "dojo/_base/array",
   "esri/layers/FeatureLayer",
+  "esri/dijit/Scalebar",
+  "dojo/parser",
   "dojo/domReady!"
-], function(Map, LayerSwipe, utils, array, FeatureLayer) {
+], function(Map, HomeButton, utils, array, FeatureLayer) {
   
    map = new Map("map", {
     basemap: "topo",
@@ -17,13 +19,6 @@
   });
   
 
-/*
-//US Percent Agriculture
-  var featureLayer = new FeatureLayer("https://services.arcgis.com/BG6nSlhZSAWtExvp/arcgis/rest/services/APHG_AgLand_update_2016/FeatureServer/0");
-    
-    map.add(featureLayer);
-    
-*/
 
  
 //Imagery Dry Year 2012
@@ -31,13 +26,19 @@
   
     map.addLayer(featureLayer2);
 
-     //Imagery Wet Year 2015
-    var featureLayer3 = new             FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2015/FeatureServer/0"); 
+//Imagery Wet Year 2015
+   var featureLayer3 = new             FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2015/FeatureServer/0"); 
   
     map.addLayer(featureLayer3);
   
-          
-            
+  
+    /*  
+  //Aquifers
+  var featureLayer3 = new FeatureLayer("https://landscape1.arcgis.com/arcgis/rest/services/USA_Aquifers/MapServer/0");
+  
+    map.addLayer(featureLayer3);
+  
+        
           var swipeWidget = new LayerSwipe({
             theme: "LayerSwipe",
             map: map,
@@ -49,8 +50,32 @@
             
          }, "LayerSwipe");
          swipeWidget.startup();
+         
+     */
+  
+  /*
+  var scalebar = new Scalebar({
+          map: map,
+      }, "Scalebar");
+          // "dual" displays both miles and kilometers
+          // "english" is the default, which displays miles
+          // use "metric" for kilometers
+          scalebarUnit: "dual"
+  });
+  
+  */
+  
+  var home = new HomeButton({
+        map: map
+      }, "HomeButton");
+      home.startup();
+  
+  var dojoConfig = {
+        parseOnLoad: true,
+        packages: [{
+          "name": "myModules",
+          "location": location.pathname.replace(/\/[^/]+$/, "") + "/myModules"
+        }]
+      };
     
   });
-
-   
-
