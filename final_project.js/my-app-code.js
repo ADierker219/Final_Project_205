c56f430f7c76a66850c4b293e10144f7d6b8fa5f
ADierker219 <35549428+ADierker219@users.noreply.github.com>
2018-05-01 18:07:15 -0600

    Push From Tuesday 5/1
    I think this will be my final push

--- final_project.js/my-app-code.js
+++ final_project.js/my-app-code.js
@@ -1,81 +1,78 @@
 
-   require([
-  "esri/map",
-  "esri/dijit/HomeButton",
-  "esri/arcgis/utils",
-  "dojo/_base/array",
-  "esri/layers/FeatureLayer",
-  "esri/dijit/Scalebar",
-  "dojo/parser",
-  "dojo/domReady!"
-], function(Map, HomeButton, utils, array, FeatureLayer) {
-  
-   map = new Map("map", {
-    basemap: "topo",
-    center: [-88.9733898, 40.063171],
-    zoom: 11
-     
- 
-  });
-  
+  require([
+    "esri/map",
+    "esri/dijit/HomeButton",
+    "esri/arcgis/utils",
+    "dojo/_base/array",
+    "esri/layers/FeatureLayer",
+    "esri/dijit/Scalebar",
+    "esri/dijit/PopupTemplate",
+    "esri/dijit/LayerSwipe",
+    "dojo/parser",
+    "dojo/domReady!"
+], function(Map, HomeButton, utils, array, FeatureLayer, Scalebar, PopupTemplate, LayerSwipe) {
 
+    map = new Map("map", {
+        basemap: "topo",
+        center: [-88.89, 40.04],
+        zoom: 3
+    });
 
- 
-//Imagery Dry Year 2012
-  var featureLayer2 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2012/FeatureServer/0");
-  
+    //Imagery Dry Year 2012
+    var popupTemplate = new PopupTemplate({
+        title: "2012 NDVI Value",
+        description: "NDVI Value = {GRIDCODE}"
+    });
+
+    var featureLayer2 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2012/FeatureServer/0",{
+        infoTemplate: popupTemplate
+    });
+
     map.addLayer(featureLayer2);
 
-//Imagery Wet Year 2015
-   var featureLayer3 = new             FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2015/FeatureServer/0"); 
-  
+    //Imagery Wet Year 2015
+    var popupTemplate2015 = new PopupTemplate({
+        title: "Imagery Wet Year 2015",
+        description: "NDVI Value = {GRIDCODE}"
+    });
+    var featureLayer3 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2015/FeatureServer/0", {
+        infoTemplate: popupTemplate2015   
+    }); 
+
     map.addLayer(featureLayer3);
+
+
+    var swipeWidget = new LayerSwipe({
+        theme: "LayerSwipe",
+        map: map,
+        layers: [featureLayer3],
+        tool: "vertical",
+        toolClip: 9,
+        toolOffsetTop: null,
+        toolOffsetLeft: null
+    }, "layerSwiper");
+    swipeWidget.startup();
+
+     
+  //Land Cover
+  var popupTemplateLandCover = new PopupTemplate({
+        title: "Land Cover",
+        description: "Land Cover Type = {Land_cover}"
+     });
   
-  
-    /*  
-  //Aquifers
-  var featureLayer3 = new FeatureLayer("https://landscape1.arcgis.com/arcgis/rest/services/USA_Aquifers/MapServer/0");
-  
-    map.addLayer(featureLayer3);
-  
-        
-          var swipeWidget = new LayerSwipe({
-            theme: "LayerSwipe",
-            map: map,
-            layers: [featureLayer2],
-            tool: "vertical",
-            toolClip: 9,
-            toolOffsetTop: null,
-            toolOffsetLeft: null
-            
-         }, "LayerSwipe");
-         swipeWidget.startup();
-         
-     */
-  
-  /*
-  var scalebar = new Scalebar({
-          map: map,
-      }, "Scalebar");
-          // "dual" displays both miles and kilometers
-          // "english" is the default, which displays miles
-          // use "metric" for kilometers
-          scalebarUnit: "dual"
+  var featureLayer = new FeatureLayer("http://services.arcgis.com/8df8p0NlLFEShl0r/arcgis/rest/services/Land_Cover/FeatureServer/0",{
+    infoTemplate: popupTemplateLandCover
   });
-  
-  */
-  
-  var home = new HomeButton({
-        map: map
-      }, "HomeButton");
-      home.startup();
-  
-  var dojoConfig = {
-        parseOnLoad: true,
-        packages: [{
-          "name": "myModules",
-          "location": location.pathname.replace(/\/[^/]+$/, "") + "/myModules"
-        }]
-      };
     
-  });
+    
+    map.addLayer(featureLayer);
+
+
+
+
+    var home = new HomeButton({
+        map: map
+    }, "HomeButton");
+    home.startup();
+
+});
