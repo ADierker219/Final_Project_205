09e40b39a42123a075f6c1382d03c9bcb5d16100
ADierker219 <35549428+ADierker219@users.noreply.github.com>
2018-05-02 12:46:27 -0600

    Final Push - Final project
    This should be good to go

--- final_project.js/my-app-code.js
+++ final_project.js/my-app-code.js
@@ -1,9 +1,4 @@
-
-<<<<<<< HEAD
-  require([
-=======
 require([
->>>>>>> e759f7e73d06518e57000e4a01852df40ad664bb
     "esri/map",
     "esri/dijit/HomeButton",
     "esri/arcgis/utils",
@@ -15,7 +10,6 @@ require([
     "dojo/parser",
     "dojo/domReady!"
 ], function(Map, HomeButton, utils, array, FeatureLayer, Scalebar, PopupTemplate, LayerSwipe) {
-<<<<<<< HEAD
 
     map = new Map("map", {
         basemap: "topo",
@@ -23,27 +17,24 @@ require([
         zoom: 3
     });
 
+  //Land Cover
+  var popupTemplateLandCover = new PopupTemplate({
+        title: "Land Cover",
+        description: "Land Cover Type = {Land_cover}"
+     });
+  
+  var featureLayer = new FeatureLayer("http://services.arcgis.com/8df8p0NlLFEShl0r/arcgis/rest/services/Land_Cover/FeatureServer/0",{
+    infoTemplate: popupTemplateLandCover
+  });
+    
+    
+    map.addLayer(featureLayer);
     //Imagery Dry Year 2012
     var popupTemplate = new PopupTemplate({
         title: "2012 NDVI Value",
         description: "NDVI Value = {GRIDCODE}"
     });
 
-=======
-
-    map = new Map("map", {
-        basemap: "topo",
-        center: [-88.89, 40.04],
-        zoom: 13
-    });
-
-    //Imagery Dry Year 2012
-    var popupTemplate = new PopupTemplate({
-        title: "Here Be the TITLE",
-        description: "And some info about this thing like the Grid Code = {GRIDCODE}"
-    });
-
->>>>>>> e759f7e73d06518e57000e4a01852df40ad664bb
     var featureLayer2 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2012/FeatureServer/0",{
         infoTemplate: popupTemplate
     });
@@ -53,17 +44,12 @@ require([
     //Imagery Wet Year 2015
     var popupTemplate2015 = new PopupTemplate({
         title: "Imagery Wet Year 2015",
-<<<<<<< HEAD
         description: "NDVI Value = {GRIDCODE}"
-=======
-        description: "And some info about this thing like the Grid Code = {GRIDCODE}"
->>>>>>> e759f7e73d06518e57000e4a01852df40ad664bb
     });
     var featureLayer3 = new FeatureLayer("https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/final_2015/FeatureServer/0", {
         infoTemplate: popupTemplate2015   
     }); 
 
-<<<<<<< HEAD
     map.addLayer(featureLayer3);
 
 
@@ -79,69 +65,13 @@ require([
     swipeWidget.startup();
 
      
-  //Land Cover
-  var popupTemplateLandCover = new PopupTemplate({
-        title: "Land Cover",
-        description: "Land Cover Type = {Land_cover}"
-     });
   
-  var featureLayer = new FeatureLayer("http://services.arcgis.com/8df8p0NlLFEShl0r/arcgis/rest/services/Land_Cover/FeatureServer/0",{
-    infoTemplate: popupTemplateLandCover
-  });
-    
-    
-    map.addLayer(featureLayer);
 
-
-
-=======
-    map.addLayer(featureLayer3);
-
-
-    var swipeWidget = new LayerSwipe({
-        theme: "LayerSwipe",
-        map: map,
-        layers: [featureLayer3],
-        tool: "vertical",
-        toolClip: 9,
-        toolOffsetTop: null,
-        toolOffsetLeft: null
-    }, "layerSwiper");
-    swipeWidget.startup();
-
-    /*  
-  //Aquifers
-  var featureLayer3 = new FeatureLayer("https://landscape1.arcgis.com/arcgis/rest/services/USA_Aquifers/MapServer/0");
-
-    map.addLayer(featureLayer3);
-
-
-
-
-     */
-
-    /*
-  var scalebar = new Scalebar({
-          map: map,
-      }, "Scalebar");
-          // "dual" displays both miles and kilometers
-          // "english" is the default, which displays miles
-          // use "metric" for kilometers
-          scalebarUnit: "dual"
-  });
-
-  */
->>>>>>> e759f7e73d06518e57000e4a01852df40ad664bb
 
     var home = new HomeButton({
         map: map
     }, "HomeButton");
     home.startup();
 
--});
+});
 
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> e759f7e73d06518e57000e4a01852df40ad664bb
